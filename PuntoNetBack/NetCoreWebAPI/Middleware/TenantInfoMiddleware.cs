091d5012afa98df5de0f61d879bf32e73fcf58ce
pablogb83 <gaionepablo@gmail.com>
2021-10-06 19:43:59 -0300

    medio roto

---
+++ PuntoNetBack/NetCoreWebAPI/Middleware/TenantInfoMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.ModeloDeDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebAPI.Middleware
+{
+    public class TenantInfoMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TenantInfoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var tenantInfo = context.RequestServices.GetRequiredService<TenantInfo>();
+            var tenantId = context.Request.Headers["Tenant"];
+
+            if (!string.IsNullOrEmpty(tenantId))
+            {
+                tenantInfo.Id = Int32.Parse(tenantId);
+
+
+                // Call the next delegate/middleware in the pipeline
+                await _next(context);
+            }
+            else
+            {
+                //responder algo
+            }
+
+        }
+    }
+}
---
+++ PuntoNetBack/Shared/ModeloDeDominio/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.ModeloDeDominio
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public byte[] Password { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public Institucion institucion { get; set; }
+    }
+}
