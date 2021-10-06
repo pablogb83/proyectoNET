385c776df840ec8621d103e5ef64db6971512b16
pablogb83 <gaionepablo@gmail.com>
2021-10-06 19:20:50 -0300

    medio roto

---
+++ PuntoNetBack/DataAccessLayer/Dtos/Instituciones/InstitucionesReadDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Dtos.Instituciones
+{
+    public class InstitucionesReadDto
+    {
+
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
+
+    }
+}
---
+++ PuntoNetBack/DataAccessLayer/Profiles/InstitucionesProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DataAccessLayer.Dtos.Instituciones;
+using Shared.ModeloDeDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Profiles
+{
+    public class InstitucionesProfile : Profile
+    {
+        public InstitucionesProfile()
+        {
+            //Source -> Target
+            CreateMap<Institucion, InstitucionesReadDto>();
+            CreateMap<InstitucionCreateDto, Institucion>();
+            CreateMap<InstitucionUpdateDto, Institucion>();
+            CreateMap<Institucion, InstitucionUpdateDto>();
+        }
+    }
+}
