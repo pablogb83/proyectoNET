a6a836db24aa4552ee926845f7345f04115d7fe2
gac1989 <germanarena12@gmail.com>
2021-10-09 13:38:29 -0300

    up multitenant

---
+++ PuntoNetBack/BusinessLayer/BL/BL_Usuario.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.IBL;
+using Shared.ModeloDeDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.BL
+{
+    public class BL_Usuario : IBL_Usuario
+    {
+        private readonly DataAccessLayer.IDAL.IDAL_Usuario _dal;
+
+        public BL_Usuario(DataAccessLayer.IDAL.IDAL_Usuario dal)
+        {
+            _dal = dal;
+        }
+
+        public Usuario Autenticar(string email, string password)
+        {
+            return _dal.Autenticar(email, password);
+        }
+
+        public void CreateUsuario(Usuario usr, string password)
+        {
+            _dal.CreateUsuario(usr, password);
+        }
+
+        public void DeleteUsuario(Usuario usr)
+        {
+            _dal.DeleteUsuario(usr);
+        }
+
+        public IEnumerable<Usuario> GetAllUsuarios()
+        {
+            return _dal.GetAllUsuarios();
+        }
+
+        public Usuario GetUsuarioById(int Id)
+        {
+            return _dal.GetUsuarioById(Id);
+        }
+
+        public bool SaveChanges()
+        {
+            return _dal.SaveChanges();
+        }
+
+        public void UpdateUsuario(Usuario usr, string password = null)
+        {
+            _dal.UpdateUsuario(usr,password);
+        }
+    }
+}
---
+++ PuntoNetBack/DataAccessLayer/IDAL/IDAL_Institucion.cs
@@ -0,0 +1,20 @@
+﻿using Shared.ModeloDeDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.IDAL
+{
+    public interface IDAL_Institucion
+    {
+        bool SaveChanges();
+
+        IEnumerable<Institucion> GetAllInstituciones();
+        Institucion GetInstitucionById(string Id);
+        void CreateInstitucion(Institucion inst);
+        void UpdateInstitucion(Institucion inst);
+        void DeleteInstitucion(Institucion inst);
+    }
+}
